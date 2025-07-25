172809d0bca228f50b8f50fe23ef2dbbc0f3fde7
ъуъ <forminecraft179@gmail.com>
2025-07-25 18:38:08 +0500

    Longest_Common_Prefix_LC14.py

--- Longest_Common_Prefix_LC14.py
+++ Longest_Common_Prefix_LC14.py
@@ -1,11 +1,1 @@
-class Solution:
-    def longestCommonPrefix(strs):
-        prefix = strs[0]         # Берём первую строку как префикс
-        for s in strs[1:]:       # Для каждой следующей строки
-            while not s.startswith(prefix):   # Пока строка не начинается с префикса
-                prefix = prefix[:-1]           # Укорачиваем префикс
-                if not prefix:                 # Если префикс закончился
-                    return ""                  # Выходим сразу
-        return prefix
-
-print(Solution.longestCommonPrefix(["flower","flow","flight"]))
+#
