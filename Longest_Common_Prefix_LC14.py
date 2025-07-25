08f6efa308d91d315ee8224082fc6146870e77c8
ъуъ <forminecraft179@gmail.com>
2025-07-25 18:48:38 +0500

    Add files via upload

--- Longest_Common_Prefix_LC14.py
+++ Longest_Common_Prefix_LC14.py
@@ -1,1 +1,10 @@
-#
+class Solution:
+    def longestCommonPrefix(self, words):
+        prefix = words[0]
+        if not words:
+            return ""
+        for word in words[1:]:
+            while not word.startswith(prefix):
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
